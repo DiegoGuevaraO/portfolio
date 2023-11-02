a1d282440d9c14205ebc4dda800c0270d5210c4f
DiegoGuevaraO <dguevaraorduna@gmail.com>
2023-11-02 09:56:00 -0600

    Layout and index page configuration

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,102 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main>
+      <section className="flex flex-col items-center justify-center h-screen  -my-20 md:-mt-48 px-8">
+            <div className="flex flex-col items-center justify-center text-center h-screen-half">
+                <img className="md:hidden object-cover w-15 h-60 rounded-lg mb-5" 
+                  src="/yo.jpeg"
+                  alt="Diego Guevara Orduña"
+                />
+                <h1 className="text-5xl sm:text-6xl lg:text-9xl">Diego Guevara</h1>
+                <h2 className="font-light text-4xl sm:text-5xl lg:text-8xl">Software Developer</h2>
+            </div>
+        </section>
+        <section className="flex items-center justify-between px-8 mb-20 tracking-wider">
+            <div className="flex flex-col w-full md:w-1/2 space-y-12 text-center md:text-left">
+                <div className="flex flex-col px-10 md:px-20">
+                    <h3 className="text-xl font-bold">Skills</h3>
+                    <br />
+                    <span className="text-lg">Management</span>
+                    <span className="text-lg">Collaboration</span>
+                    <span className="text-lg">Communication</span>
+                </div>
+                <div className="px-10 md:px-20">
+                    <h3 className="text-xl font-bold">Summary</h3>
+                    <br />
+                    <p className="w-full md:w-2/3">Information Technology Engineer with experience in web development and deep knowledge in Python, HTML and JavaScript.</p>
+                </div>
+                <div className="px-10 md:px-20">
+                    <br />
+                    <h3 id='contact' className="text-xl font-bold">Contact</h3>
+                    <a className="text-xl hover:text-blue-600" href="mailto:dguevaraorduna@gmail.com">dguevaraorduna@gmail.com</a>
+                    <p>834 180 2619</p>
+                </div>
+            </div>
+            <img className="hidden md:block object-cover w-2/5 h-screen rounded-lg"
+                src="/yo.jpeg"
+                alt="Diego Guevara Orduña"
             />
-          </a>
+        </section>
+        <hr className="border-gray-400 mx-44" />
+        <section className="px-20 mt-10">
+            <h4 id='experience' className="mb-8 text-3xl font-bold text-center md:text-left">Experience</h4>
+            <div className="grid grid-cols-1 md:grid-cols-6 my-10">
+                <div className="flex flex-col col-span-2 mb-4 md:mb-0">
+                    <h5 className="text-xl md:text-2xl font-bold">Web Developer</h5>
+                    <h6 className="text-lg font-bold">High Technologies Analytics</h6>
+                    <p>Dec 2022 - present</p>
+                </div>
+                <div className="flex flex-col col-span-4">
+                    <p>Currently working on the development and maintenance of the iTool 2.0
+                        platform, which has 21,000 stores where evaluations are carried out daily.
+                        Making use of Python, JavaScript, Bootstrap, and cartography frameworks.</p>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-6 my-10">
+                <div className="flex flex-col col-span-2 mb-4 md:mb-0">
+                    <h5 className="text-xl md:text-2xl  font-bold">Database Analyst,</h5>
+                    <h6 className="text-lg font-bold">Cinvestav Unidad Tamaulipas</h6>
+                    <p>May 2022 - Sep 2022</p>
+                </div>
+                <div className="flex flex-col col-span-4">
+                    <p>Data extraction for creating a graph database, report generation, and
+                        implementing machine learning algorithms to extract relevant information.</p>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-6 my-10">
+                <div className="flex flex-col col-span-2 mb-4 md:mb-0">
+                    <h5 className="text-xl md:text-2xl  font-bold">Software Developer</h5>
+                    <h6 className="text-lg font-bold">Subsecretaría de Innovación y Tecnologías de la Información</h6>
+                    <p>Sep 2019 - Dec 2019</p>
+                </div>
+                <div className="flex flex-col col-span-4">
+                    <p>Implementation and documentation of the Single Administration and Finance
+                        System performing Web service and database migration as well as
+                        maintenance of computer equipment.</p>
+                </div>
+            </div>
+        </section>
+        <hr className="border-gray-400 mx-44" />
+        <section className="px-20 mt-10">
+            <h4 className="mb-8 text-3xl font-bold text-center md:text-left">Education</h4>
+            <div className="grid grid-cols-1 md:grid-cols-6 my-10">
+                <div className="flex flex-col col-span-2 mb-4 md:mb-0">
+                    <h5 className="text-xl md:text-2xl  font-bold">Information Technology Engineering</h5>
+                    <h6 className="text-lg font-bold">Universidad Politécnica de Victoria</h6>
+                    <p>2018 - 2022</p>
+                </div>
+                <div className="flex flex-col col-span-4 ">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eveniet deleniti tempore veritatis
+                        adipisci accusantium voluptatibus vel aperiam ex alias officiis deserunt, ad, iste id cum minus sit
+                        laudantium ullam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi non incidunt
+                        voluptates molestiae delectus nulla quisquam aperiam voluptas tempora distinctio! Ipsa cupiditate
+                        harum voluptates praesentium. Suscipit itaque officiis odio ut!</p>
+                </div>
+            </div>
+        </section>
+        <div className="h-80">
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
